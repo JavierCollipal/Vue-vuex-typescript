7875f78bbeac803f5f4e7d398266915a0ff47ca2
jcollipal <jcollipal1212@gmail.com>
2019-11-20 12:32:49 -0300

    develop updated

--- src/namespaces/Http.ts
+++ src/namespaces/Http.ts
@@ -1,35 +1,40 @@
-import Vue from 'vue';
-import { AxiosRequestConfig, AxiosResponse, AxiosInstance } from 'axios';
+import Vue from "vue";
+import { AxiosRequestConfig, AxiosResponse, AxiosInstance } from "axios";
 declare module "vue/types/vue" {
-    interface VueConstructor {
-        axios: AxiosInstance;
-    }
+  interface VueConstructor {
+    axios: AxiosInstance;
+  }
 }
 export namespace Http {
-    export class ApiJwtService {
-        constructor() {
-            this.request();
-            this.response();
-        }
-
-        protected request() {
-            Vue.axios.interceptors.request.use((config: AxiosRequestConfig) => {
-                console.log('AXIOS REQUEST')
-                return config;
-            }, (err: any) => {
-                console.log("AXSIOS REQUEST ERROR");
-                return Promise.reject(err);
-            })
-        }
-        protected response() {
-
-            Vue.axios.interceptors.response.use((response: AxiosResponse) => {
-                console.log('AXIOS RESPONSE')
-                return response;
-            }, (err: any) => {
-                console.log("AXSIOS RESPONSE ERROR");
-                return Promise.reject(err);
-            })
-        }
+  export class ApiJwtService {
+    constructor() {
+      this.request();
+      this.response();
     }
+
+    protected request() {
+      Vue.axios.interceptors.request.use(
+        (config: AxiosRequestConfig) => {
+          console.log("AXIOS REQUEST");
+          return config;
+        },
+        (err: any) => {
+          console.log("AXSIOS REQUEST ERROR");
+          return Promise.reject(err);
+        }
+      );
+    }
+    protected response() {
+      Vue.axios.interceptors.response.use(
+        (response: AxiosResponse) => {
+          console.log("AXIOS RESPONSE");
+          return response;
+        },
+        (err: any) => {
+          console.log("AXSIOS RESPONSE ERROR");
+          return Promise.reject(err);
+        }
+      );
+    }
+  }
 }
